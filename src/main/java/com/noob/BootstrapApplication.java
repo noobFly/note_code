8b87b3db22b24efd4f263e665be5e4fb25296db7
xiongwenjun <xiongwenjun@utrust.cn>
2021-04-14 12:44:12 +0800

    update

--- src/main/java/com/noob/BootstrapApplication.java
+++ src/main/java/com/noob/BootstrapApplication.java
@@ -1,8 +1,6 @@
 package com.noob;
 
-import java.util.List;
 
-import org.apache.flink.shaded.guava18.com.google.common.collect.Lists;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -18,12 +16,7 @@ import com.noob.spring.beanDefinition.CustomizerClientImportBeanDefinitionRegist
 //@Import(BeanDefinitionRegistrarForImport.class)
 public class BootstrapApplication {
 	public static void main(String[] args) {
-		
-		List<String>  list = Lists.newArrayList();
-		list.add("a");
-		for(String a : list) {
-			System.out.println(a);
-		}
+	
 		SpringApplication.run(BootstrapApplication.class, args);
 	}
 }
